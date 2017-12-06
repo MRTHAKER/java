9cb14e64781336a3eed63d75eaad1ef15b1e332d
MRTHAKER <mahek.haker@gmail.com>
2017-12-07 01:16:21 +0530

    simple java program with some if conditions to provide spanish translation from 1 to 5 with the command line argument

---
+++ tl.java
@@ -0,0 +1,36 @@
+class tl
+{
+
+public static void main(String args[])
+{
+
+int k;
+k = Integer.parseInt(args[0]);
+
+if(k==1)
+{
+System.out.println("uno");
+}
+
+if(k==2)
+{
+System.out.println("dos");
+}
+
+if(k==3)
+{
+System.out.println("tres");
+}
+
+if(k==4)
+{
+System.out.println("quarto");
+}
+
+if(k==5)
+{
+System.out.println("cinco");
+}
+
+}
+}
