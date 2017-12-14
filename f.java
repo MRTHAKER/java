d7f0ff0e8eff04a21c75415b4cac2a4e03ec3fab
MRTHAKER <mahek.haker@gmail.com>
2017-12-14 19:06:13 +0530

    comparing of two numbers.
    comparing values with passing command line arguments for dynamic initialization of variables
    this is helpful and making your code more powerful

---
+++ f.java
@@ -0,0 +1,21 @@
+class f
+{
+
+public static void main(String args[])
+{
+
+int a,b;
+a=Integer.parseInt(args[0]);
+b=Integer.parseInt(args[1]);
+System.out.println("a="+a);
+System.out.println("b="+b);
+if (a<b)
+{
+System.out.println("b is big");
+}
+else
+{
+System.out.println("a is big");
+}
+}
+}
