7a5d929715cbba3882e8143e26b175e613409cbf
MRTHAKER <mahek.haker@gmail.com>
2017-12-14 19:07:57 +0530

    swapping of two numbers this includes old method of swapping of numbers in the simplest way

---
+++ k.java
@@ -0,0 +1,17 @@
+class k
+{
+public static void main(String args[])
+{
+int a=10;
+int b=20;
+int c=0;
+System.out.println("values before swap a="+a);
+System.out.println("values before swap b="+b);
+c=a;
+a=b;
+b=c;
+System.out.println("values before swap a="+a);
+System.out.println("values before swap b="+b);
+}
+}
+
