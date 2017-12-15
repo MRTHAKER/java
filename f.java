96c8b8d8882dd18a04bd5d6e5fa7a155f9547450
MRTHAKER <mahek.haker@gmail.com>
2017-12-15 19:19:03 +0530

    update program code to use proper OOP structure create object in other class named f and rename class f to p

--- f.java
+++ f.java
@@ -1,12 +1,9 @@
-class f
-{
-
-public static void main(String args[])
+class p
 {
 
 int a,b;
-a=Integer.parseInt(args[0]);
-b=Integer.parseInt(args[1]);
+void lol()
+{
 System.out.println("a="+a);
 System.out.println("b="+b);
 if (a<b)
@@ -17,5 +14,17 @@ else
 {
 System.out.println("a is big");
 }
+}
+}
+
+
+class f
+{
+public static void main(String args[])
+{
+p bro=new p();
+bro.a=Integer.parseInt(args[0]);
+bro.b=Integer.parseInt(args[1]);
+bro.lol();
 }
 }
