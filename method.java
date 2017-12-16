de80b62f832f5604ce113776239ee709b005d9ae
MRTHAKER <mahek.haker@gmail.com>
2017-12-16 18:44:13 +0530

    method overloading with parameters a simple method overloading program to make it more interesting we using a sax named "this" "this" keyword refers to current object and helps in reducing code lines

---
+++ method.java
@@ -0,0 +1,35 @@
+class k
+{
+int a,b,c;
+void lol(int a,int b,int c)
+{
+this.a=a;
+this.b=b;
+this.c=c;
+
+this.a=this.a+1;
+this.b=this.b+2;
+this.c=this.c+3;
+System.out.println("values after  sax"+this.a);
+System.out.println("values after  sax"+this.b);
+System.out.println("values after  sax"+this.c);
+}
+void lol(int a,int b)
+{
+this.a=b;
+this.b=b;
+int ans;
+ans=this.a*this.b;
+System.out.println("overloaded Answer is"+this.a*this.b);
+}
+}
+class method
+{
+public static void main(String args[])
+{
+k pro=new k();
+pro.lol(10,20,30);
+pro.lol(2,3);
+}
+}
+
