fe868cc2665882bd764fdfdf47ad520c6bd874e1
MRTHAKER <mahek.haker@gmail.com>
2017-12-16 18:56:31 +0530

    Add more sax in program make it more confusing for noobs of class to understand this is added to avoid kanging of code just another program of homework make code more flexible to avoid making 2 seperate programs because i am too lazy to write two programs lol();

--- method.java
+++ method.java
@@ -22,6 +22,14 @@ int ans;
 ans=this.a*this.b;
 System.out.println("overloaded Answer is"+this.a*this.b);
 }
+void lol(k ob)
+{
+int a=ob.a;
+int b=ob.b;
+int ans=2;
+ans=a+b+ans;
+System.out.println("Answer is"+ans);
+}
 }
 class method
 {
@@ -30,6 +38,8 @@ public static void main(String args[])
 k pro=new k();
 pro.lol(10,20,30);
 pro.lol(2,3);
+k bro=new k();
+bro.lol(pro);
 }
 }
 
