d70c3ccc92ca08478ce413b61cef433d6b25510a
MR.HACKER <mahek.haker@gmail.com>
2018-02-23 19:38:05 +0530

    simple button program with java applet

---
+++ dra.java
@@ -0,0 +1,29 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/* <applet code="dra" height=300 width=300>
+</applet>*/
+
+public class dra extends Applet implements ActionListener
+{
+Label l1;
+Button b1,b2;
+public void init()
+{
+b1=new Button ("MR");
+b2=new Button("5476");
+
+l1=new Label("                            ");
+add(b1);
+add(b2);
+add(l1);
+b1.addActionListener(this);
+b2.addActionListener(this);
+}
+public void actionPerformed(ActionEvent b)
+{
+l1.setText(b.getActionCommand());
+}
+}
+
