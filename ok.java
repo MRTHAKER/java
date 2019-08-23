c8b0fc8327354ee13930d3f9a7e35cb31cc648f4
MRTHAKER <mahek.haker@gmail.com>
2019-08-23 19:17:00 +0530

    Java: Update String example to count vowels also

--- ok.java
+++ ok.java
@@ -4,13 +4,20 @@ public static void main(String args[]){
    Scanner sc = new Scanner(System.in);
    System.out.println("Enter String to find vowels");
    String s=sc.nextLine();
+   int count=0;
    for(int i=0;i<s.length();i++){
-   if(s.contains("a") || s.contains("e") || s.contains("i") || s.contains("o") || s.contains("u")){
+   if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u') 
+   {
+	   count++;
+   }   
+   }
    s=s.replaceAll("a","*");
    s=s.replaceAll("e","*");
    s=s.replaceAll("i","*");
    s=s.replaceAll("o","*");
    s=s.replaceAll("u","*");
-   }
+System.out.println("Number of vowels found in your String are: "+count);
+System.out.println(s);
     }
-System.out.println(s);}}
+
+}
