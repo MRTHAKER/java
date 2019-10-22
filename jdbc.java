a3e2a96615965c911b78e8f990beb73d39e77cd5
MR.5476 <mahek.haker@gmail.com>
2019-10-22 10:31:17 +0530

    Java: Update redirection

--- jdbc.java
+++ jdbc.java
@@ -4,25 +4,27 @@ import java.sql.*;
 import java.io.*;
 
 public class jdbc extends HttpServlet{
-public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException,ServletException{
+public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException,ServletException{
 try{
 PrintWriter out=res.getWriter();
 String url="jdbc:mysql://localhost:3306/mahek";
 String uname="root";
 String pass="Sumo@123";
-String nm="arpit";
-int ps=234;
 Class.forName("com.mysql.jdbc.Driver");
+String nm=req.getParameter("nm");
+int ps=Integer.parseInt(req.getParameter("ps"));
 Connection con =DriverManager.getConnection(url,uname,pass);
 PreparedStatement st=con.prepareStatement("insert into login values(?,?)");
 st.setString(1,nm);
 st.setInt(2,ps);
-//ResultSet rs=
-st.executeUpdate();
-/*while(rs.next()){
-out.println(rs.getString(1)+" "+rs.getInt(2));*/
+int k=st.executeUpdate();
 con.close();
+if(k>0){
+res.sendRedirect("http://localhost:9090/mahek/display");
+}
+else{
+out.println("error");}
 }
 catch(Exception e){}
-}//end of doGet()
+}//end of doPost()
 }//end of class
