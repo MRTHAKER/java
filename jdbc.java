6a5d3ddc6600e637a8f1fbc6b53af2eb00329002
MRTHAKER <mahek.haker@gmail.com>
2019-11-25 15:49:51 +0530

    Java: Update insert code

--- jdbc.java
+++ jdbc.java
@@ -4,7 +4,7 @@ import java.sql.*;
 import java.io.*;
 
 public class jdbc extends HttpServlet{
-public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException,ServletException{
+public void doPost(HttpServletRequest req, HttpServletResponse res){
 try{
 PrintWriter out=res.getWriter();
 String url="jdbc:mysql://localhost:3306/mahek";
@@ -12,18 +12,13 @@ String uname="root";
 String pass="Sumo@123";
 Class.forName("com.mysql.jdbc.Driver");
 String nm=req.getParameter("nm");
-int ps=Integer.parseInt(req.getParameter("ps"));
+String ps=req.getParameter("ps");
 Connection con =DriverManager.getConnection(url,uname,pass);
 PreparedStatement st=con.prepareStatement("insert into login values(?,?)");
 st.setString(1,nm);
-st.setInt(2,ps);
-int k=st.executeUpdate();
-con.close();
-if(k>0){
+st.setString(2,ps);
+st.executeUpdate();
 res.sendRedirect("http://localhost:9090/mahek/display");
-}
-else{
-out.println("error");}
 }
 catch(Exception e){}
 }//end of doPost()
